450acc1bb97a8c615ec451e1db4bc8e8c3749e88
eternium1337 <177254494+eternium1337@users.noreply.github.com>
2025-02-09 10:15:11 +0300

    aimbot.h

--- aimbot.h
+++ aimbot.h
@@ -3,7 +3,7 @@ Standoff2 0.32.2
 void *(get_bipedmap)(void*); // il2cpp + 0x27049F0
 void AIMBOT()
 {
-    void *v0 = *(void*)((uint64_t)il2cppstartaddr + 0x5139E30 +0x20 + 0xB8 + 0x70);
+    void *v0 = *(void*)((uint64_t)il2cppstartaddr + 0x5139E30 + 0x20 + 0xB8 + 0x70);
     if (v0)
     {
         void *v2 = *(void*)((uint64_t)il2cppstartaddr + 0x5139E30 + 0x20 + 0xB8);
@@ -26,7 +26,7 @@ void AIMBOT()
                         *(float *)(v45 + 0x18) = AimAngle;
                         *(float *)(v45 + 0x1C) = AimAngle;
                         *(float *)(v45 + 0x20) = AimAngle;
-                        *(float *)(v45 +0x24) = AimAngle;
+                        *(float *)(v45 + 0x24) = AimAngle;
                         *(float *)(v45 + 0x28) = AimAngle;
                         *(float *)(v45 + 0x2C) = AimAngle;
                     }
