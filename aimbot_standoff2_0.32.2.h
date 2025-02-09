aa9b787815b17187db0d02a5ab53feda9fd03078
eternium1337 <177254494+eternium1337@users.noreply.github.com>
2025-02-09 10:30:07 +0300

    aimbot_standoff2_0.32.2.h

--- aimbot_standoff2_0.32.2.h
+++ aimbot_standoff2_0.32.2.h
@@ -17,18 +17,20 @@ void AIMBOT()
                     {
                         void* PlayerHead = *(void**)((uint64_t)get_bipedmap(enemy) + 0x20);
                         Vector3 Target = get_position(get_transform(PlayerHead)) - get_position(get_transform(get_main()));
-                        float AimAngle = Vector3::Angle(Target, get_forward(get_transform(get_main())) * 150);
-                        v45 = *(void*)((uint64_t)v0 + 0x60 + 0x90);
-                        if (AimAngle >= 275.0)
-                            AimAngle = AimAngle - 360.0;
-                        if (AimAngle <= -275.0)
-                            AimAngle = AimAngle + 360.0;
-                        *(float *)(v45 + 0x18) = AimAngle;
-                        *(float *)(v45 + 0x1C) = AimAngle;
-                        *(float *)(v45 + 0x20) = AimAngle;
-                        *(float *)(v45 + 0x24) = AimAngle;
-                        *(float *)(v45 + 0x28) = AimAngle;
-                        *(float *)(v45 + 0x2C) = AimAngle;
+                        float AimAngle = Vector3::Angle(Target, get_forward(get_transform(get_main())) * 300);
+                        if (AimAngle <= float(AimBotFoV) / 700.0f) {
+                            v45 = *(void*)((uint64_t)v0 + 0x60 + 0x90);
+                            if (AimAngle >= 275.0)
+                                AimAngle = AimAngle - 360.0;
+                            if (AimAngle <= -275.0)
+                                AimAngle = AimAngle + 360.0;
+                            *(float *)(v45 + 0x18) = AimAngle;
+                            *(float *)(v45 + 0x1C) = AimAngle;
+                            *(float *)(v45 + 0x20) = AimAngle;
+                            *(float *)(v45 + 0x24) = AimAngle;
+                            *(float *)(v45 + 0x28) = AimAngle;
+                            *(float *)(v45 + 0x2C) = AimAngle;
+                        }
                     }
                 }
             }
